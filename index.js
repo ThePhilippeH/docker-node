d98fc3afc8947ecd67b94c46a98c733bc0287673
philippe henry <pipe.henry@gmail.com>
2024-04-28 20:33:15 +0200

    update mongodb container

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {res.send("weii?")});
+app.get("/", (req, res) => {res.send("wadup?")});
 
 const port = process.env.PORT || 3000; // Port 3000 is the default port
 
