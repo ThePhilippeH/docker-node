c1ecf911acec1660e961d36727a10cb4127bec0d
philippe henry <pipe.henry@gmail.com>
2024-04-29 17:00:36 +0200

    Addition of Mongoose for data access

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");//the db manager
 
 const app = express();
+
+//connect to the db using the @mongo for ip address because of dns resolution
+mongoose
+.connect("mongodb://phil:pass@mongo:27017/?authSource=admin")
+.then(()=>{console.log("connected to the DB")})
+.catch((e)=>  {console.log("error connecting to the DB", e)});
 
 app.get("/", (req, res) => {res.send("wadup?")});
 
