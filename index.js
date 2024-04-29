8e2f59fb8fe16522ae043e9d0548e9c404ef38f4
philippe henry <pipe.henry@gmail.com>
2024-04-29 17:55:46 +0200

    added logic for app dependencies on mongoDB

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-//connect to the db
+const connectWithRetry = () => {
+  //connect to the db
 mongoose
-.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+.connect(mongoURL, {})
 .then(()=>{console.log("connected to the DB")})
-.catch((e)=>  {console.log("error connecting to the DB", e)});
+.catch((e)=>{console.log("error connecting to the DB", e)
+setTimeout(connectWithRetry, 5000)}); //keep trying to connect every 5 seconds
+}
+
+connectWithRetry();
 
 app.get("/", (req, res) => {res.send("wadup?")});
 
