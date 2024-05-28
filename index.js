da97c94a98356dda7dce82005f4c4984b88bc315
philippe henry <pipe.henry@gmail.com>
2024-05-28 15:36:06 +0200

    Switching over to mongo sessions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");//the db manager
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const boulderRouter = require("./routes/boulderRoutes");
@@ -10,24 +12,43 @@ const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 const connectWithRetry = () => {
-  //connect to the db
-mongoose
-.connect(mongoURL)
-.then(()=>{console.log("connected to the DB")})
-.catch((e)=>{console.log("error connecting to the DB", e)
-setTimeout(connectWithRetry, 5000)}); //keep trying to connect every 5 seconds
-}
+  mongoose
+    .connect(mongoURL)
+    .then(() => {
+      console.log("connected to the DB");
+      app.use(
+        session({
+          secret: "unsecreto",
+          resave: false,
+          saveUninitialized: true,
+          store: MongoStore.create({ mongoUrl: mongoURL }),
+          cookie: {
+            secure: false,
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24,
+            sameSite:"lax"
+          },
+        })
+      );
+    })
+    .catch((e) => {
+      console.log("error connecting to the DB", e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
 connectWithRetry();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {res.send("boulder?")});
+app.get("/", (req, res) => {
+  res.send("boulder?");
+});
 
 app.use("/api/v1/boulders", boulderRouter);
 app.use("/api/v1/users", userRouter);
 
-const port = process.env.PORT || 3000; // Port 3000 is the default port
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
